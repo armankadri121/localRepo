5e76936f210ee8b5c57122adf199750d5aa7d62b
Aarman K Kadri <aarman1922030@gmail.com>
2025-06-28 14:48:50 +0530

    Updated hashing code

--- hashing.cpp
+++ hashing.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 int main() {
     int arr[] = {1,3,7,7,15};
     int n = sizeof(arr) / sizeof(arr[0]);
     int q = 4;
+    int max = *max_element(arr, arr+n);
+    cout << max;
 
-    int hash[16] = {0};
+    int hash[max+1] = {0};
     for(int i=0; i<n; i++) {
         hash[arr[i]] += 1;
     }
